706b2a8a23dfe6f16b7145e13903aa4cdf63ddda
olegosipenko <oleg.s.osipenko@gmail.com>
2015-08-02 16:06:41 +0300

    License

--- PTTextView/src/main/java/pttextview/style/PTTypefaceSpan.java
+++ PTTextView/src/main/java/pttextview/style/PTTypefaceSpan.java
@@ -1,4 +1,19 @@
-package pttextviewq.style;
+/* Copyright 2015 Oleg Osipenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pttextview.style;
 
 import android.content.Context;
 import android.graphics.Typeface;
@@ -6,8 +21,8 @@ import android.support.annotation.NonNull;
 import android.text.TextPaint;
 import android.text.style.MetricAffectingSpan;
 
-import pttextviewq.utils.PTTextViewUtils;
-import pttextviewq.utils.PTTypefaceManager;
+import pttextview.utils.PTTextViewUtils;
+import pttextview.utils.PTTypefaceManager;
 
 /**
  * Created by Oleg on 19.07.15.
